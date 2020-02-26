40fabad978d3372d033ae97fe2aba4bb402c1fd1
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-02-26 19:27:02 +0900

    Add Main

--- src/main/java/global/sesoc/tempcat/controller/SampleController.java
+++ src/main/java/global/sesoc/tempcat/controller/SampleController.java
@@ -4,17 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import global.sesoc.tempcat.dao.DAO;
 
 @Controller
 public class SampleController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-//	@Autowired
-//	private DAO dao;
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
+    @Autowired
+    private DAO dao;
 
+    @GetMapping(value = "single")
+    public String single() {
+	return "temp/single";
+    }
+
+    @GetMapping(value = "index")
+    public String index() {
+	return "temp/index";
+    }
 }
